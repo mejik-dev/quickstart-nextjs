a64b22d967b3383f77bbb2aacb9b30328ec83c24
habrhm <m.haris.abdurrahman@gmail.com>
2022-09-20 13:52:27 +0700

    feat: init quickstart

---
+++ README.md
@@ -0,0 +1,3 @@
+# microgen Quickstart NextJS
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/microgen-quickstart-nextjs)
---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { useRouter } from "next/router";
+import Login from "../components/Login";
+import Register from "../components/Register";
+import { microgen } from "../lib/microgen";
+
+const Home = () => {
+  const router = useRouter();
+  const [switchAuthForm, setSwitchAuthForm] = React.useState("login");
+
+  React.useEffect(() => {
+    (async () => {
+      const { error } = await microgen.auth.user();
+
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      router.push("/profile");
+    })();
+  }, []);
+
+  const handleRegisterForm = () => {
+    setSwitchAuthForm("register");
+  };
+
+  const handleLoginForm = () => {
+    setSwitchAuthForm("login");
+  };
+
+  return (
+    <div className="auth-page">
+      <div className="auth-button">
+        <button onClick={handleLoginForm}>Login</button>
+        <button onClick={handleRegisterForm}>Register</button>
+      </div>
+      {switchAuthForm === "register" ? <Register /> : <Login />}
+    </div>
+  );
+};
+
+export default Home;
